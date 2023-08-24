992543a0408e1a1056ed56295fe3f78ae87c4e2b
Venkateswarareddy19-07 <112817526+Venkateswarareddy19-07@users.noreply.github.com>
2023-08-24 10:42:23 +0530

    Add files via upload

---
+++ sum of fibbonaci series.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a=0,b=1,range,c,sum=0;
+	printf("enter the range of numbers:");
+	scanf("%d",&range);
+	printf("the fibonacci series of numbers are:\n");
+	while(a<=range){
+		printf("%d\t",a);
+		sum+=a;
+		c=a+b;
+		a=b;
+		b=c;
+	}
+	printf("\n their sum is=%d",sum);
+	return 0;
+}
